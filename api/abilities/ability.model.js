0014c5431e95264e36a1d8d28fb4aa28e609053c
Alejandro Paz <alepazmont@gmail.com>
2024-04-10 01:48:12 +0200

    fix abilities

--- api/abilities/ability.model.js
+++ api/abilities/ability.model.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const abilitySchema = new mongoose.Schema({
     name: { type: String, required: true, trim: true, unique: true },
-    description: { type: String },
-    type: { type: String }, // Type of ability (e.g., Physical, Energy-based, etc.)
-    characters: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Character' }] // Cast members (reference to Character model)
+    description: { type: String, required: true },
+    type: { type: String, required: true }, 
+    characters: { type: Array, required: true }
 });
 
 const Ability = mongoose.model("Ability", abilitySchema);
