325647f66bf52045423fc3a337ad36579b6a4fde
Alejandro Paz <alepazmont@gmail.com>
2024-04-11 16:12:39 +0200

    query params

--- api/characters/character.router.js
+++ api/characters/character.router.js
@@ -1,7 +1,5 @@
 const express = require("express");
-
 const characterRouter = express.Router();
-
 const {
     create,
     getOne,
@@ -9,15 +7,15 @@ const {
     getOneByName,
     updateOne,
     deleteOne
-} = require("./character.controller")
+} = require("./character.controller");
+const { isAuth } = require("../middleware/auth.middleware");
 
-const { isAuth } = require("../middleware/auth.middleware")
+// Rutas
+characterRouter.post("/", [isAuth], create);
+characterRouter.get("/:id", getOne); // Ruta para obtener un personaje por su ID
+characterRouter.get("/", getAll); // Ruta para obtener todos los personajes
+characterRouter.get("/search", getOneByName); // Ruta para buscar personajes por nombre
+characterRouter.patch("/:id", [isAuth], updateOne); // Ruta para actualizar un personaje por su ID
+characterRouter.delete("/:id", [isAuth], deleteOne); // Ruta para eliminar un personaje por su ID
 
-characterRouter.post("/", [isAuth], create)
-characterRouter.get("/:id", getOne)
-characterRouter.get("/", getAll)
-characterRouter.get("/search", getOneByName);
-characterRouter.patch("/:id", [isAuth], updateOne)
-characterRouter.delete("/:id", [isAuth], deleteOne)
-
-module.exports = characterRouter
+module.exports = characterRouter;
